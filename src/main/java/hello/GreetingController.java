904443ddd00e120f170680e466a2e32e5d4d84b7
jaimerg <jaimerg21@gmail.com>
2015-02-13 17:58:16 +0100

    sfs

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -13,8 +13,10 @@ public class GreetingController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        System.out.println("hola");
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
+
 
     }
 }
